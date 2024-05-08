3ad70ba4a4ae6641c64a162f63d9b4ffba83922b
mohammad mohammadzade <42555452+mohammadzadeh74@users.noreply.github.com>
2024-05-08 13:14:50 -0500

    Update query.py

--- query.py
+++ query.py
@@ -1,7 +1,6 @@
 from llama_index.query_engine import RetrieverQueryEngine
 from llama_index.vector_stores import PineconeVectorStore
 from llama_index.retrievers import VectorIndexRetriever
-import env_template
 import openai
 import os
 from pinecone.grpc import PineconeGRPC
@@ -10,12 +9,20 @@ from llama_index import VectorStoreIndex
 import argparse
 
 
-#Initialisation
-openai.api_key = "Enter-Your-OpenAI-Key"
-pc = PineconeGRPC(api_key="Enter-Your-Pinecone-Key")
-pc_index = pc.Index("rag")
-vector_store = PineconeVectorStore(pinecone_index=pc_index)
-vec_ind = VectorStoreIndex.from_vector_store(vector_store=vector_store)
+def query(question):
+    #Initialisation
+    openai.api_key = "Enter-Your-OpenAI-Key"
+    pc = PineconeGRPC(api_key="Enter-Your-API-Key")
+    pc_index = pc.Index("rag")
+    vector_store = PineconeVectorStore(pinecone_index=pc_index)
+    vec_ind = VectorStoreIndex.from_vector_store(vector_store=vector_store)
+    index_retriever = VectorIndexRetriever(index=vec_ind, similarity_top_k=8)
+    result = index_retriever.retrieve(question)
+    query_engine = RetrieverQueryEngine(retriever=index_retriever)
+    llm_query = query_engine.query(question)
+    return(llm_query.response)
+
+
 
 
 #Main Part
@@ -23,9 +30,4 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Query")
     parser.add_argument("--query", type=str, required=True, help="Enter your question")
     args = parser.parse_args()
-    index_retriever = VectorIndexRetriever(index=vec_ind, similarity_top_k=8)
-    result = index_retriever.retrieve(args.query)
-    query_engine = RetrieverQueryEngine(retriever=index_retriever)
-    llm_query = query_engine.query(args.query)
-    print(llm_query.response)
-
+    print(query(args.query))
